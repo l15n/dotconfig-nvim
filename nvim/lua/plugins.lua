9621602f6eecde8df038d62b5cd66f09eb44a805
Leonard Chin <l.g.chin@gmail.com>
2023-11-11 17:31:36 +0900

    Add basic LSP config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -40,4 +40,22 @@ require('pckr').add{
   -- color scheme
   'fenetikm/falcon';
   'rose-pine/neovim';
+
+  { 'VonHeikemen/lsp-zero.nvim',
+    branch = 'v3.x',
+    requires = {
+	    {'williamboman/mason.nvim'},
+	    {'williamboman/mason-lspconfig.nvim'},
+	    -- LSP Support
+	    {'neovim/nvim-lspconfig'},
+	    -- Autocompletion
+	    {'hrsh7th/nvim-cmp'},
+	    {'hrsh7th/cmp-nvim-lsp'},
+	    {'hrsh7th/cmp-path'},
+	    {'hrsh7th/cmp-buffer'},
+	    -- Snippets
+	    {'saadparwaiz1/cmp_luasnip'},
+	    {'L3MON4D3/LuaSnip'},
+    },
+  };
 }
