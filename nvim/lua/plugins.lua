3055bc129b8e8e6ed16140e3b91d1a4f2074423f
Leonard Chin <l.g.chin@gmail.com>
2023-11-11 17:49:25 +0900

    Add basic treesitter config

--- nvim/lua/plugins.lua
+++ nvim/lua/plugins.lua
@@ -58,4 +58,12 @@ require('pckr').add{
 	    {'L3MON4D3/LuaSnip'},
     },
   };
+
+  { 'nvim-treesitter/nvim-treesitter',
+    run = function()
+      local ts_update = require('nvim-treesitter.install').update({ with_sync = true })
+      ts_update()
+    end,
+  };
+  --'nvim-treesitter/playground';
 }
