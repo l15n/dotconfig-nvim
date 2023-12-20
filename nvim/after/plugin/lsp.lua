fb79e9d246d1271a99565c519e97f38f6211750a
Leonard Chin <3714+l15n@users.noreply.github.com>
2023-12-20 22:17:22 +0900

    basic completion mappings

--- nvim/after/plugin/lsp.lua
+++ nvim/after/plugin/lsp.lua
@@ -6,6 +6,27 @@ lsp_zero.on_attach(function(client, bufnr)
   lsp_zero.default_keymaps({buffer = bufnr})
 end)
 
+  local cmp = require('cmp')
+  local cmp_action = require('lsp-zero').cmp_action()
+
+  cmp.setup({
+    window = {
+      completion = cmp.config.window.bordered(),
+      documentation = cmp.config.window.bordered(),
+    },
+    sources = {
+      {name = 'nvim_lsp'},
+    },
+    mapping = cmp.mapping.preset.insert({
+      ['<C-Space>'] = cmp.mapping.complete(),
+      ['<C-f>'] = cmp_action.luasnip_jump_forward(),
+      ['<C-b>'] = cmp_action.luasnip_jump_backward(),
+      ['<C-u>'] = cmp.mapping.scroll_docs(-4),
+      ['<C-d>'] = cmp.mapping.scroll_docs(4),
+      ['<C-y>'] = cmp.mapping.confirm({ select = true }),
+    })
+  })
+
 require('mason').setup({})
 require('mason-lspconfig').setup({
   ensure_installed = { 'lua_ls', 'ruby_ls', 'tsserver' },
