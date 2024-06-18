f8c315376bc8e387c2741bd3616d99a1eb7a9faf
Leonard Chin <3714+l15n@users.noreply.github.com>
2024-06-18 17:35:18 +0900

    Set vim-test strategy

--- nvim/after/plugin/vim-test.lua
+++ nvim/after/plugin/vim-test.lua
@@ -1,2 +1,4 @@
 vim.keymap.set("n", "<Leader>t", "<cmd>TestNearest<cr>", { silent = true })
 vim.keymap.set("n", "<Leader>T", "<cmd>TestFile<cr>", { silent = true })
+
+vim.g['test#strategy'] = 'neovim_sticky'
